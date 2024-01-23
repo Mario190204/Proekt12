012ca45a4048f20399504b7095a645f21395cb99
Mario1902 <dimitrievskimario@gmail.com>
2024-01-23 11:35:04 +0100

    Proekt12

---
+++ src/nova/Vehicle.java
@@ -0,0 +1,22 @@
+package nova;
+
+public class Vehicle {
+
+	private String color;
+
+	Vehicle() {
+        color = "Blue"; // Set a default color, e.g., "Blue"
+    }
+
+    // Constructor with a parameter to set the color
+    Vehicle(String a) {
+        color = a;
+    }
+
+    }
+
+
+	
+	
+
+
